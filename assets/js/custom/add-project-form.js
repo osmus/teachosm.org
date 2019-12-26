ed36567b8509b13a93ee2dce285d68bcbdf5334d
@tomgertin <tgertin@vt.edu>
2019-12-26 16:42:04 -0500

    Update add-project-form.js

--- assets/js/custom/add-project-form.js
+++ assets/js/custom/add-project-form.js
@@ -206,6 +206,7 @@ const submitForm = async () => {
       title,
       type,
       url: `${now}-${parseInt(Math.random() * 1000000)}`,
+      g-recaptcha-response: grecaptcha.getResponse(),
     };
 
     const pullRequestResponse = await axios.post(
